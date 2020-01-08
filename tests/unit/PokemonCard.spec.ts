407dda4fc49750a4e4173b7cc5a6b65f057b6156
luiztsmelo <luiztsmelo@gmail.com>
2020-01-08 17:42:40 -0300

    pokemonCard test added

--- tests/unit/PokemonCard.spec.ts
+++ tests/unit/PokemonCard.spec.ts
@@ -1,27 +1,32 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue  } from '@vue/test-utils';
+import VueRouter from 'vue-router';
 import PokemonCard from '@/components/PokemonCard.vue';
 
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+const router = new VueRouter();
+
 describe('PokemonCard.vue', () => {
-  test('renders correctly', () => {
-    const wrapper = shallowMount(PokemonCard, {
-      mocks: {
-        $route: {
-          query: {
-            q: 'Bulbasaur',
-          }
-        }
+  const wrapper = shallowMount(PokemonCard, {
+    localVue,
+    router,
+    propsData: {
+      pokemon: {
+        id: 'UG9rZW1vbjowMDE=',
+        number: '001',
+        name: 'Bulbasaur',
+        types: [
+          'Grass', 'Poison',
+        ],
       },
-      propsData: {
-        pokemon: {
-          id: 'UG9rZW1vbjowMDE=',
-          number: '001',
-          name: 'Bulbasaur',
-          types: [
-            'Grass', 'Poison'
-          ]
-        }
-      }
-    })
+    },
+  });
+  test('renders correctly', () => {
     expect(wrapper.element).toMatchSnapshot()
-  })
+  });
+  test('route query changes correctly on card click', () => {
+    const pokemonCard = wrapper.find('.pokemon-card');
+    pokemonCard.trigger('click');
+    expect(wrapper.vm.$route.query.q).toBe('Bulbasaur');
+  });
 });
